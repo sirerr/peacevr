d1d6e171f4cea07e4d2d14dfd50ae6dc9c049fe2
sirerr <sirerr@gmail.com>
2015-07-26 21:23:08 -0400

    quick upload

---
+++ peacevr/Assets/Scripts/playercontrol.cs
@@ -0,0 +1,21 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class playercontrol : MonoBehaviour {
+
+	public bool touchpadinput = false;
+	public bool backbuttoninput = false;
+
+	// Use this for initialization
+	void Start () {
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		touchpadinput = Input.GetMouseButton(0);
+		backbuttoninput = Input.GetMouseButton(1);
+
+	}
+}
