f023e1abdd7fa7904d703cfefee89b764def4763
sirerr <sirerr@gmail.com>
2015-08-05 22:26:26 -0400

    got power meter working

---
+++ peacevr/Assets/Scripts/game/UIcode.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class UIcode : MonoBehaviour {
+
+	public RawImage energybarobj;
+	public Texture[] energybars;
+	//playerref
+	public playerstats playerstatsref;
+	public float energylevel=0;
+	public int intenergylevel =0;
+
+
+	// Use this for initialization
+	void Start () {
+	
+
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+		energylevel = Mathf.Abs(playerstatsref.playerenergy/10f);
+		intenergylevel = Mathf.FloorToInt(energylevel);
+
+
+		switch(intenergylevel)
+		{
+		case 10:
+			energybarobj.texture = energybars[9];
+			break;
+		case 9:
+			energybarobj.texture = energybars[8];
+			break;
+		case 8:
+			energybarobj.texture = energybars[7];
+			break;
+		case 7:
+			energybarobj.texture = energybars[6];
+			break;
+		case 6:
+			energybarobj.texture = energybars[5];
+			break;
+		case 5:
+			energybarobj.texture = energybars[4];
+			break;
+		case 4:
+			energybarobj.texture = energybars[3];
+			break;
+		case 3:
+			energybarobj.texture = energybars[2];
+			break;
+		case 2:
+			energybarobj.texture = energybars[1];
+			break;
+		case 1:
+			energybarobj.texture = energybars[0];
+			break;
+		default:
+			energybarobj.texture = null;
+			break;
+		}
+
+	}
+}
